2421ec53845bffcb8d753545f33c5d6cf3b7493f
yenchu-ntu-csie <b11902003@csie.ntu.edu.tw>
2025-06-05 16:05:45 +0800

    Uncomment initialization commands in init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
-# pip freeze | xargs pip uninstall -y
-# pip install -r requirements.txt
-# cd grep
-# python page.py
-# python activity.py
-# python activity_html.py
-# python content.py
-# cd ..
+pip freeze | xargs pip uninstall -y
+pip install -r requirements.txt
+cd grep
+python page.py
+python activity.py
+python activity_html.py
+python content.py
+cd ..
 find . -name "*.pkl" -delete
 python method2/orginal.py -m item_cold_start
 python method2/baseline.py -m baseline
