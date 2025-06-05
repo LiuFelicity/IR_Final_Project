fd96f63f4a777587f664e6045964722037070ddb
yenchu-ntu-csie <b11902003@csie.ntu.edu.tw>
2025-06-05 17:00:33 +0800

    [fix] Add missing command to execute fetch_missing_activities.py in init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,18 @@
 pip freeze | xargs pip uninstall -y
 pip install -r requirements.txt
+
 cd grep
 python page.py
 python activity.py
 python activity_html.py
 python content.py
+
+cd ..
+python grep/fetch_missing_activities.py
+
+cd grep
+python content.py
+
 cd ..
 find . -name "*.pkl" -delete
 python method2/orginal.py -m item_cold_start
